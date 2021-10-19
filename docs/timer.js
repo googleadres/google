4cc0ea155b0b05819a69dd941df47b01c07dde8a
googleadres <google@adres.pl>
2021-10-19 11:40:03 +0200

    next

--- docs/timer.js
+++ docs/timer.js
@@ -2,7 +2,7 @@
 function odliczanie()
 {
     var dzisiaj = new Date();
-    //var rok = rok.
+    rok = dzisiaj.getFullYear();
     var dzien   = dzisiaj.getDate();
     if (dzien < 10) dzien = "0"+dzien;
     var miesiac = dzisiaj.getMonth()+1;
@@ -13,7 +13,7 @@ function odliczanie()
     if (minuta < 10) minuta = "0"+minuta;
     var sekunda = dzisiaj.getSeconds();
     if (sekunda < 10) sekunda = "0"+sekunda;
-    document.getElementById("zegar").innerHTML = dzien+"/"+miesiac+" || Godzina: "+godzina+":"+minuta+":"+sekunda;
+    document.getElementById("zegar").innerHTML = dzien+"/"+miesiac+"/"+rok+" || Godzina: "+godzina+":"+minuta+":"+sekunda;
 
     setTimeout("odliczanie()",1000);
 }
